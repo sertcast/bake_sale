d28deca30df3d52c33f5420d85ba6a52d1e98799
Mert Kaya <30465847+sertcast@users.noreply.github.com>
2017-11-12 14:53:36 -0500

    Initial Commit

---
+++ bake_sale/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bake_sale
+//
+//  Created by Mert Kaya on 2017-11-12.
+//  Copyright © 2017 Mert Kaya. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
